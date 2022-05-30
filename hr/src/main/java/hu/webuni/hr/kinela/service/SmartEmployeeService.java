4ce53f8501779877a01dc6d911e1b1efe3af21a9
Laszlo Kineth <laszlo.kineth1@gmail.com>
2022-05-30 22:23:34 +0200

    getPayRaisePercent method is modified in SmartEmloyeeService.

--- hr/src/main/java/hu/webuni/hr/kinela/service/SmartEmployeeService.java
+++ hr/src/main/java/hu/webuni/hr/kinela/service/SmartEmployeeService.java
@@ -1,6 +1,7 @@
 package hu.webuni.hr.kinela.service;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,73 +22,20 @@ public class SmartEmployeeService implements EmployeeService {
 	 */
 	
 	@Autowired
-	EmployeeConfigurationProperties properties; // = new EmployeeConfigurationProperties();
+	EmployeeConfigurationProperties properties;
 
 	@Override
 	public int getPayRaisePercent(EmployeeDto employee) {
 
-		LocalDateTime startDate = employee.getStartDateOfWork();
-
-		// Make years and salary percentage aggregation
+		double yearsWorked = (ChronoUnit.DAYS.between(employee.getStartDateOfWork(), LocalDateTime.now()) / 365);
 		
-		if (startDate.isBefore(LocalDateTime.now().minusYears(properties.getYears().getMax()))) {
-			return properties.getPercent().getMax();
-		} else if (startDate.isBefore(LocalDateTime.now().minusYears(properties.getYears().getMid()))
-				&& (startDate.isAfter(LocalDateTime.now().minusYears(properties.getYears().getMax())))) {
+		if (yearsWorked > properties.getYears().getMax()) {
+ 			return properties.getPercent().getMax();
+		} else if (yearsWorked > properties.getYears().getMid() && yearsWorked < properties.getYears().getMax()) {
 			return properties.getPercent().getMid1();
-		} else if (startDate.isBefore(LocalDateTime.now().minusYears(properties.getYears().getMin()))
-				&& (startDate.isAfter(LocalDateTime.now().minusYears(properties.getYears().getMid())))) {
+		} else if (yearsWorked > properties.getYears().getMin() && yearsWorked < properties.getYears().getMid()) {
 			return properties.getPercent().getMid2();
-		} else if (startDate.isAfter(LocalDateTime.now().minusYears(properties.getYears().getMin()))) {
-			return properties.getPercent().getMin();
-		} else {
-			return properties.getPercent().getMin();
-		}
-
+		} else return properties.getPercent().getMin();
 	}
 
 }
-
-/**
- * Direct value from application.properties
- */
-
-//	@Value("${hr.years.max}")
-//	int employeeYearsMax;
-//	
-//	@Value("${hr.years.mid}")
-//	int employeeYearsMid;
-//
-//	@Value("${hr.years.min}")
-//	int employeeYearsMin;
-//
-//	@Value("${hr.percent.max}")
-//	int employeePercentsMax;
-//
-//	@Value("${hr.percent.mid1}")
-//	int employeePercentsMid1;
-//	
-//	@Value("${hr.percent.mid2}")
-//	int employeePercentsMid2;
-//	
-//	@Value("${hr.percent.min}")
-//	int employeePercentsMin;
-
-//	@Override
-//	public int getPayRaisePercent(Employee employee) {
-//
-//		LocalDateTime startDate = employee.getStartDateOfWork();
-//		
-//		if (startDate.isBefore(LocalDateTime.now().minusYears(employeeYearsMax))) {
-//			return employeePercentsMax;
-//		} else if (startDate.isBefore(LocalDateTime.now().minusYears(employeeYearsMid)) && (startDate.isAfter(LocalDateTime.now().minusYears(employeeYearsMax)))) {
-//			return employeePercentsMid1;
-//		} else if (startDate.isBefore(LocalDateTime.now().minusYears(employeeYearsMin)) && (startDate.isAfter(LocalDateTime.now().minusYears(employeeYearsMid)))) {
-//			return employeePercentsMid2;
-//		} else if (startDate.isAfter(LocalDateTime.now().minusYears(employeeYearsMin))) {
-//			return employeePercentsMin;
-//		} else {
-//			return 0;
-//		}
-//		
-//	}
