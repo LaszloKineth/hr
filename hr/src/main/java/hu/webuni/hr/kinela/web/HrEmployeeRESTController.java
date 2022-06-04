16e30d0800fe903cc56199de9d32712b5cf79467
Laszlo Kineth <laszlo.kineth1@gmail.com>
2022-06-04 23:52:11 +0200

    Unnecessary if/else statements are removed.

--- hr/src/main/java/hu/webuni/hr/kinela/web/HrEmployeeRESTController.java
+++ hr/src/main/java/hu/webuni/hr/kinela/web/HrEmployeeRESTController.java
@@ -24,73 +24,87 @@ import hu.webuni.hr.kinla.dto.EmployeeDto;
  *
  */
 
-
 @RestController
 @RequestMapping("/api/employees")
 public class HrEmployeeRESTController {
-		
-		@GetMapping
-		public List<EmployeeDto> getAllEmployees() {
-			
-			return Employees.getEmployeesList();
-		}
-		
-		@GetMapping("/{id}")
-		public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable long id) {
 
-			EmployeeDto employee = Employees.getEmployees().get(id);
-			
-			if (employee != null) {
-				return ResponseEntity.ok(employee);
-			} else {
-				return ResponseEntity.notFound().build();
-			}
-		}
-		
-		@PostMapping
-		public EmployeeDto createEmployee(@RequestBody EmployeeDto employee) {
-			
-			Employees.addEmployee(employee);
-			
-			return employee;
-		}
-		
-		@PutMapping("/{id}")
-		public ResponseEntity<EmployeeDto> modifíEmployee(@PathVariable long id, @RequestBody EmployeeDto employee) {
+//		1. version
+//	
+//		@GetMapping
+//		public List<EmployeeDto> getAllEmployees() {
+//			
+//			return Employees.getEmployeesList();
+//		}
+//		
+//		@GetMapping(params = "min_salary")
+//		public ResponseEntity<List<EmployeeDto>> getEmployeesWithGreaterSalary(@RequestParam(required = false) int min_salary) {
+//			
+//			List<EmployeeDto> higherSalaryEmployees = new ArrayList<>();
+//			
+//			for (EmployeeDto employeeDto : new ArrayList<EmployeeDto>(Employees.getEmployees().values())) {
+//					
+//				if (employeeDto.getSalary() >  min_salary) {
+//						higherSalaryEmployees.add(employeeDto);
+//				}
+//			}
+//				
+//			if (higherSalaryEmployees.isEmpty()) {
+//				return ResponseEntity.noContent().build();
+//			} else {
+//				return ResponseEntity.ok(higherSalaryEmployees);
+//			}
+//
+//		}
 
-			if(!Employees.getEmployees().containsKey(id)) {
-				return ResponseEntity.noContent().build();
-			} else {
-				employee.setId(id);
-				Employees.modifyEmployee(id, employee);
-				
-				return ResponseEntity.ok(employee);
-			}
-		}
-
-		@DeleteMapping("/{id}")
-		public void deleteEmployee(@PathVariable long id) {
-			Employees.getEmployees().remove(id);
-		}
 	
-		@GetMapping(params = "min_salary")
-		public ResponseEntity<List<EmployeeDto>> getEmployeesWithGreaterSalary(@RequestParam int min_salary) {
+// 2. version
+	@GetMapping
+	public ResponseEntity<List<EmployeeDto>> getAllEmployees(@RequestParam(required = false) Integer min_salary) {
 
+		if (min_salary == null) {
+			return ResponseEntity.ok(Employees.getEmployeesList());
+
+		} else {
 			List<EmployeeDto> higherSalaryEmployees = new ArrayList<>();
-			
+
 			for (EmployeeDto employeeDto : new ArrayList<EmployeeDto>(Employees.getEmployees().values())) {
-					
-				if (employeeDto.getSalary() >  min_salary) {
-						higherSalaryEmployees.add(employeeDto);
+
+				if (employeeDto.getSalary() > min_salary) {
+					higherSalaryEmployees.add(employeeDto);
 				}
 			}
-				
-			if (higherSalaryEmployees.isEmpty()) {
-				return ResponseEntity.noContent().build();
-			} else {
-				return ResponseEntity.ok(higherSalaryEmployees);
-			}
 
+			return ResponseEntity.ok(higherSalaryEmployees);
 		}
+
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable long id) {
+
+		return ResponseEntity.ok(Employees.getEmployees().get(id));
+	}
+
+	@PostMapping
+	public EmployeeDto createEmployee(@RequestBody EmployeeDto employee) {
+
+		Employees.addEmployee(employee);
+
+		return employee;
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<EmployeeDto> modifíEmployee(@PathVariable long id, @RequestBody EmployeeDto employee) {
+
+		employee.setId(id);
+		Employees.modifyEmployee(id, employee);
+
+		return ResponseEntity.ok(employee);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteEmployee(@PathVariable long id) {
+		Employees.getEmployees().remove(id);
+	}
 
 }
